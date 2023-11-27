7b3af5510c78859cdda82b3e50d1ea53e27ea66e
acorriaz <mikey.swx@gmail.com>
2023-11-27 10:58:30 +0700

    Add all ex

--- ex-1.js
+++ ex-1.js
@@ -1,3 +1,15 @@
 // Exercise #1: Food Order
 
 // Start coding here
+const foodOrder = {
+  orderNumber: "A0234",
+  address: "Bangkok",
+  paymentType: "cash",
+  totalPrice: 4500,
+  restaurantName: "mk",
+};
+
+foodOrder.totalPrice = 5000;
+foodOrder.paymentType = "Credit Card";
+
+console.log(foodOrder);
